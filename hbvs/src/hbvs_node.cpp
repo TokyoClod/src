a105e067c0403a67049763b515cabec46966e3e8
abner <793907563@qq.com>
2018-05-08 11:46:39 +0800

    fix logZ and theatureU feature

--- hbvs/src/hbvs_node.cpp
+++ hbvs/src/hbvs_node.cpp
@@ -125,7 +125,7 @@ int main(int argc, char** argv)
     ros::param::get("~lambdaoo", lambdaoo);
     ros::param::get("~lambda0_d", lambda0_d);
     vpAdaptiveGain lambda(lambda0,lambdaoo,lambda0_d);
-    task.setLambda(0.75);
+    task.setLambda(0.5);
 
     double controller_P,controller_I,controller_D;
     ros::param::get("~controller_P", controller_P);
@@ -168,14 +168,14 @@ int main(int argc, char** argv)
 
 
     /********************** Second feature log (Z/Zd) ***********************/
-    // #pragma region secondfeature
-    // vpGenericFeature logZd(1);
-    // logZd.set_s(log(Zd));
-    // double x = 0; //The x coordinate of the current point.
-    // double y = 0; //The y coordinate of the current point.
-    // double Z = 5; //The depth of the current point.
-    // vpGenericFeature logZ(1); //The dimension of the feature is 1.
-    // logZ.set_s( log(Z) );
+    #pragma region secondfeature
+    vpGenericFeature logZd(1);
+    logZd.set_s(log(Zd));
+    double x = 0; //The x coordinate of the current point.
+    double y = 0; //The y coordinate of the current point.
+    double Z = 5; //The depth of the current point.
+    vpGenericFeature logZ(1); //The dimension of the feature is 1.
+    logZ.set_s( log(Z) );
 
 
     #pragma endregion secondfeature
@@ -184,11 +184,12 @@ int main(int argc, char** argv)
     /********************** 3rd feature ThetaU ***********************/
     #pragma region thirdfeature
     // vpPoseVector cd_r_o(0, 0, Zd, vpMath::rad(0), vpMath::rad(0), vpMath::rad(0)); //need to fix
-    vpHomogeneousMatrix cdMu(vpTranslationVector(0, 0, -Zd + 0.12), vpRotationMatrix(0, 0, 0));//vpRzyxVector(-1.5708, 0,  3.1416)
+    vpHomogeneousMatrix cdMu(vpTranslationVector(0, 0, -Zd), vpRotationMatrix(vpRzyxVector(0, 0, 0)));//vpRzyxVector(-1.5708, 0,  3.1416)
+    cout<< "cdMu:\n" << cdMu << endl;
     // vpHomogeneousMatrix cdMo(cd_r_o);
     vpHomogeneousMatrix cdMc;
-    cdMc = cdMu * (wMu.inverse()) * wMc;
-    vpFeatureThetaU tu(vpFeatureThetaU::cdRc);
+    cdMc = cdMu * (wMu.inverse() * wMc);
+    vpFeatureThetaU tu(vpFeatureThetaU::cRcd);
     tu.buildFrom(cdMc);
     #pragma endregion thirdfeature
 
@@ -198,21 +199,21 @@ int main(int argc, char** argv)
         plotter.setTitle(0, "Visual features error");
         plotter.setTitle(1, "Camera velocities");
 
-        plotter.initGraph(0, 3);
+        plotter.initGraph(0, 12);
         plotter.initGraph(1, 6);
 
-        // plotter.setLegend(0, 0, "x1");
-        // plotter.setLegend(0, 1, "y1");
-        // plotter.setLegend(0, 2, "x2");
-        // plotter.setLegend(0, 3, "y2");
-        // plotter.setLegend(0, 4, "x3");
-        // plotter.setLegend(0, 5, "y3");
-        // plotter.setLegend(0, 6, "x4");
-        // plotter.setLegend(0, 7, "y4");
-        // plotter.setLegend(0, 8, "Z");
-        plotter.setLegend(0, 0, "tu0");
-        plotter.setLegend(0, 1, "tu1");
-        plotter.setLegend(0, 2, "tu2");
+        plotter.setLegend(0, 0, "x1");
+        plotter.setLegend(0, 1, "y1");
+        plotter.setLegend(0, 2, "x2");
+        plotter.setLegend(0, 3, "y2");
+        plotter.setLegend(0, 4, "x3");
+        plotter.setLegend(0, 5, "y3");
+        plotter.setLegend(0, 6, "x4");
+        plotter.setLegend(0, 7, "y4");
+        plotter.setLegend(0, 8, "Z");
+        plotter.setLegend(0, 9, "tu0");
+        plotter.setLegend(0, 10, "tu1");
+        plotter.setLegend(0, 11, "tu2");
 
         plotter.setLegend(1, 0, "v_x");
         plotter.setLegend(1, 1, "v_y");
@@ -231,10 +232,10 @@ int main(int argc, char** argv)
     for (unsigned int i = 0 ; i < 4 ; i++) {
         // vpFeatureBuilder::create(pd[i], cam, point[i]);
         // vpFeatureBuilder::create(pp[i], cam, point[i]);
-        // task.addFeature(pp[i], pd[i]);
+        task.addFeature(pp[i], pd[i]);
         cout<<"pd["<<i<<"]=:"<<pd[i].get_x()<<","<<pd[i].get_y()<<","<<pd[i].get_Z()<<endl;
     }
-    // task.addFeature(logZ, logZd);
+    task.addFeature(logZ, logZd);
     task.addFeature(tu);
     task.print();
     //ibvs ends
@@ -312,17 +313,17 @@ int main(int argc, char** argv)
                 //cout<<"pp["<<i<<"]=:"<<pp[i].get_x()<<","<<pp[i].get_y()<<","<<pp[i].get_Z()<<endl;   
             }
 
-            cdMc = cdMu * (wMu.inverse()) * wMc;
-            cout<< "cdMc:\n" << cdMc << endl;
+            cdMc = cdMu * (wMu.inverse() * wMc);
+            // cout<< "cdMc:\n" << cdMc << endl;
             tu.buildFrom(cdMc);
 
-            // logZ.set_s(log(Z));
-            // vpMatrix LlogZ(1, 6);
-            // LlogZ[0][0] = LlogZ[0][1] = LlogZ[0][5] = 0;
-            // LlogZ[0][2] = -1 / Z;
-            // LlogZ[0][3] = -pp[0].get_y();
-            // LlogZ[0][4] = pp[0].get_x();
-            // logZ.setInteractionMatrix(LlogZ);
+            logZ.set_s(log(Z));
+            vpMatrix LlogZ(1, 6);
+            LlogZ[0][0] = LlogZ[0][1] = LlogZ[0][5] = 0;
+            LlogZ[0][2] = -1 / Z;
+            LlogZ[0][3] = -pp[0].get_y();
+            LlogZ[0][4] = pp[0].get_x();
+            logZ.setInteractionMatrix(LlogZ);
             
             // pp[4].buildFrom(double((det->c[0]-u0) / cam.get_px()), double((det->c[1]-v0) / cam.get_py()), Z);
             delete det;                     
@@ -358,6 +359,7 @@ int main(int argc, char** argv)
             vpVelocityTwistMatrix fVc;
             //fVc.buildFrom((wMc*cMo).inverse() * wMu);
             fVc.buildFrom((wMc*wfc)); //transfer the velocity from camera frame to Fixed World Frame
+            // cout << "fvc:\n" << fVc << endl;
             vpColVector f_v(6);
             // vpColVector f_omega(3);
             //cout<<endl<<"fVc:\n"<<fVc<<endl<<endl;
@@ -365,11 +367,11 @@ int main(int argc, char** argv)
             f_v = fVc * v;
             // f_omega = - 0.5 * fVc * (tu_uav - tu_ugv);
             
-            // vel_skew.twist.linear.x = f_v[0];
-            // vel_skew.twist.linear.y = f_v[1];
-            // vel_skew.twist.linear.z = f_v[2];
-            //vel_skew.twist.angular.x = f_v[3];
-            //vel_skew.twist.angular.y = f_v[4];
+            vel_skew.twist.linear.x = f_v[0];
+            vel_skew.twist.linear.y = f_v[1];
+            vel_skew.twist.linear.z = f_v[2];
+            // vel_skew.twist.angular.x = f_v[3];
+            // vel_skew.twist.angular.y = f_v[4];
             vel_skew.twist.angular.z = f_v[5];
 
             #ifdef VISP_HAVE_DISPLAY
